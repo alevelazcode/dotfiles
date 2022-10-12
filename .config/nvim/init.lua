b1a3859e939d669e6fa8dd2d5644e41116bc6c1f
alejandro <alejandro@11.0.16.172.net-uno.net>
2022-10-12 17:30:53 -0400

    fix config

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,14 +1,19 @@
 require("config.base")
 require("config.highlights")
-require("config.maps" )
-require ("config.plugins")
+require("config.maps")
+require("config.plugins")
 
 local has = function(x)
   return vim.fn.has(x) == 1
 end
 local is_mac = has "macunix"
 local is_win = has "win32"
+local is_linux = has "unix" and not has "macunix"
 
 if is_mac then
+  require('config.macos')
+end
+
+if is_linux then
   require('config.macos')
 end
