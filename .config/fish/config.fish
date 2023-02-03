bd33b22dba5c8ca0e2dc061b77b4bccfd81df174
alejandro velazco <alejanvelazco2008@hotmail.com>
2023-02-03 10:38:08 -0300

    .

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -69,7 +69,6 @@ alias algo-node="ssh root@159.89.181.135"
 alias testnet-node="ssh root@137.184.44.109"
 alias wallet-dev-="ssh root@159.65.221.171"
 alias wallet-prod="ssh vendible@137.184.202.212"
-alias cd="z"
 # export PATH="$PATH:$HOME/.spicetify"
 
 
