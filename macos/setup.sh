c94069fff1fdd02cd9478bcbf3063f1206a22ae9
Alejandro <alejanvelazco2008@hotmail.com>
2023-10-06 11:27:48 -0300

    include tab

--- macos/setup.sh
+++ macos/setup.sh
@@ -8,13 +8,18 @@ echo "Updating Brew"
 brew update
 brew upgrade
 
+brew tap Homebrew/bundle
+
+#
 # Install from Brewfile
 echo "Installing from Brewfile"
 brew bundle --file=~/dotfiles/macos/Brewfile
 
+# Set fish as default shell
 echo $(which fish) | sudo tee -a /etc/shells
 chsh -s $(which fish)
 
+#
 # Install fish plugins with fisher 
 echo "Installing fish plugins with fisher"
 fish -c "fisher update"
@@ -25,9 +30,10 @@ fish -c "fisher add jethrokuan/fzf"
 
 
 
+
 # Linked files config
 echo "Linking files"
-ln -s ~/dotfiles/fish/config.fish ~/.config/fish/config.fish
+ln -s ~/dotfiles/fish/* ~/.config/fish/
 
 ln -s  ../common/neofetch ~/.config
 ln -s ../common/startship/starship.toml ~/.config
@@ -55,3 +61,13 @@ echo "<--- Installing rustup ---> "
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 # Check rust version
 rustup --version
+
+
+
+
+# Install miniconda3
+mkdir -p ~/miniconda3
+curl https://repo.anaconda.com/miniconda/Miniconda3-latest-MacOSX-arm64.sh -o ~/miniconda3/miniconda.sh
+bash ~/miniconda3/miniconda.sh -b -u -p ~/miniconda3
+rm -rf ~/miniconda3/miniconda.sh
+
