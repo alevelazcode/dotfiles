5cdf307d4a4b6fde4742a7b6ac9050842c15bda0
Alejandro <alejanvelazco2008@hotmail.com>
2023-11-17 13:52:45 -0300

    feature: include zsh config with zap

--- unix/zsh/functions.zsh
+++ unix/zsh/functions.zsh
@@ -21,17 +21,4 @@ function dcu() { docker compose up -d }
 
 
 
-function run_roger_api {
-  export TWILIO_AUTH_TOKEN=6fe113093f0e40265f93d8b96f3bee16                                                                             
-  export TWILIO_ACCT_SID=AC34af568071949f09acdeefacf038ad65
-  export TWILIO_NUM=+15005550006
-  export MAILCHIMP_API_KEY=f8e893bedcf96d4929c984bf6a6a6db8-us21
-  yarn api
-}
-
-function run_roger_dashboard {
-  export NODE_ENV=development                                                                                                   
-  export NX_TWILIO_NUM=+15005550006
-  yarn dashboard
-}
 
